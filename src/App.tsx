606925afcc755cc287ba3324b084bfb18f26b561
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-09-13 11:39:08 -0400

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,30 @@ function App() {
       <h1>CS5610 : Web Development </h1>
         <h2> Northeastern University</h2> 
         <h2>Fall 2024</h2>
+        <br/>
+        <br/>
+        <br/>
+        <h2>Name : Somesh M Bachani</h2>
+        <h2>Section : 1</h2>
+        <br/>
+        <br/>
+        <br/>
+        <p>Relavant links: <a
+          className="App-link"
+          href="https://a1--cs5610kanbas.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kanbas APP
+        </a></p>
+        <p>Relavant links: <a
+          className="App-link"
+          href="https://github.com/BachaniS/kanbas-react-web-app/tree/a1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a></p>
       </header>
     </div>
   );
