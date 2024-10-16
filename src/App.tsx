8ccdc8be37ff237241038a2353baf2fa1d1c5015
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-10-16 05:09:05 -0400

    Refactor CSS styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>CS5610 : Web Development </h1>
-        <h2> Northeastern University</h2> 
+        <h1>CS5610: Web Development</h1>
+        <h2>Northeastern University</h2>
         <h2>Fall 2024</h2>
-        <br/>
-        <br/>
-        <br/>
-        <h2>Name : Somesh M Bachani</h2>
-        <h2>Section : 1</h2>
-        <br/>
-        <br/>
-        <br/>
-        <p>Relavant links: <a
-          className="App-link"
-          href="https://a3--cs5610kanbas.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kanbas APP || 
-        </a><a
-          className="App-link"
-          href="https://github.com/BachaniS/kanbas-react-web-app/tree/a2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >  Github </a>
+        <div className="spacer" />
+        <h2>Name: Somesh M Bachani</h2>
+        <h2>Section: 1</h2>
+        <div className="spacer" />
+        <p>
+          Relevant links: 
+          <a
+            className="App-link"
+            href="https://a3--cs5610kanbas.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kanbas APP
+          </a> 
+          {' || '}
+          <a
+            className="App-link"
+            href="https://github.com/BachaniS/kanbas-react-web-app/tree/a2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
         </p>
       </header>
     </div>
