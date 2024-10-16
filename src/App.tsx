92bb9fd01b8e06b3a8194eabb747658edea20a5a
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-10-16 16:16:58 -0400

    Refactor CSS styles in App component to update background color and remove unnecessary spacing

--- src/App.tsx
+++ src/App.tsx
@@ -73,10 +73,6 @@ function App() {
               </a>
               <figcaption>Labs</figcaption>
             </figure>
-            <br />
-            <br />
-            <br />
-            <br />
           </div>
         </div>
       </header>
