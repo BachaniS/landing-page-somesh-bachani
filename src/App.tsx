4b88a99f28a548fdd16aebdd2aebc0703ac021ca
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-10-16 05:46:52 -0400

    Refactor CSS styles in App component to increase the size of logo images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -13,32 +12,45 @@ function App() {
         <h2>Section: 1</h2>
         <div className="spacer" />
         <p>
-          Relevant links: 
-            <a
+          Relevant links:
+          <a
             className="App-link"
             href="https://a3--cs5610kanbas.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
-            >
+          >
             <img
               src="https://www.instructure.com/sites/default/files/image/2021-12/Canvas_logo_single_mark.png"
               alt="Canvas Logo"
-              style={{ width: '40px', height: '40px' }}
+              style={{ width: "40px", height: "40px" }}
             />
-            </a>
-          {' || '}
-            <a
+          </a>
+          {" || "}
+          <a
             className="App-link"
             href="https://github.com/BachaniS/kanbas-react-web-app/tree/a2"
             target="_blank"
             rel="noopener noreferrer"
-            >
+          >
             <img
               src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
               alt="GitHub"
-              style={{ width: '40px', height: '40px' }}
+              style={{ width: "30px", height: "30px" }}
             />
-            </a>
+          </a>
+          {" || "}
+          <a
+            className="App-link"
+            href="https://a3--cs5610kanbas.netlify.app/#/Labs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://cdn3.vectorstock.com/i/1000x1000/53/72/labs-logo-inspiration-vector-23065372.jpg"
+              alt="Labs"
+              style={{ width: "30px", height: "30px" }}
+            />
+          </a>
         </p>
       </header>
     </div>
