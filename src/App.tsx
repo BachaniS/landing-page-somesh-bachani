811a3d3a0f34c3d7de7f699c6a6ed20f1165ae94
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-10-16 05:24:21 -0400

    Refactor CSS styles in App component for better readability and contrast

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,31 @@ function App() {
         <div className="spacer" />
         <p>
           Relevant links: 
-          <a
+            <a
             className="App-link"
             href="https://a3--cs5610kanbas.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            Kanbas APP
-          </a> 
+            >
+            <img
+              src="https://www.instructure.com/sites/default/files/image/2021-12/Canvas_logo_single_mark.png"
+              alt="Canvas Logo"
+              style={{ width: '20px', height: '20px' }}
+            />
+            </a>
           {' || '}
-          <a
+            <a
             className="App-link"
             href="https://github.com/BachaniS/kanbas-react-web-app/tree/a2"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            Github
-          </a>
+            >
+            <img
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt="GitHub"
+              style={{ width: '20px', height: '20px' }}
+            />
+            </a>
         </p>
       </header>
     </div>
